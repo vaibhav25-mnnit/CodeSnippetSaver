7cdd30631d36ac490799a87c4be566cf0a885b9f
vaibhav25-mnnit <bagatevaibhav555@gmail.com>
2022-07-29 11:42:04 +0530

    change the palcehoder

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -84,7 +84,7 @@ export default function Dashboard() {
                                     }}>
                                         <InputGroup className="mb-3" size="lg">
                                             <Form.Control
-                                                placeholder="Add new Topic"
+                                                placeholder="Add new Idea"
                                                 value={topic}
                                                 onChange={(e) => {
                                                     setTopic(e.target.value);
